eddda3c4a3416d5a69776d44c13e7d5f00ec7bb0
Julian Eisenmann <1810653115@stud.fh-kufstein.ac.at#>
2018-11-08 14:01:44 +0100

    Aufgabe 4

--- src/package3/Main3.java
+++ src/package3/Main3.java
@@ -4,13 +4,23 @@ public class Main3
 {
     public static void main (String[] args)
     {
+        // Aufgabe 3
+        // Datentyp festlegen, mit Namen befüllen und ausgeben
         String name;
         name = "Julian Eisenmann";
         System.out.println(name);
 
+        // Datentyp festlegen, mit Alter befüllen und ausgeben
         int alter;
         alter = 23;
         System.out.println(alter);
 
+        //Aufgabe 4
+        //String fachhochschuleKufsteinTirol = "Fachhochschule Kufstein Tirol";
+        //Verkürzung Variablenname von fachhochschuleKufsteinTirol zu fhKufsteinTirol
+        //String fhKufsteinTirol = "Fachhochschule Kufstein Tirol";
+        //Verwandlung Variable in Kosntante und Umbenennung in FH_KUFSTEIN_TIROL
+         final String FH_KUFSTEIN_TIROL = "Fachhochschule Kufstein Tirol";
     }
+
 }
