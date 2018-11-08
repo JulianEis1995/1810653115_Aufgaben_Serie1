1cfc7ecc55f4a97a529a417ca5f1a9e7813fccf2
Julian Eisenmann <1810653115@stud.fh-kufstein.ac.at#>
2018-11-08 13:42:04 +0100

    Hinzufügen von Kommentaren

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,17 @@
+//Erstellt eine Klasse Main
 public class Main
 {
+    //Erstellt die ausführbare Main Methode
     public static void main(String[] args)
     {
+        //Ausgabe "Hello World" auf Konsole
         System.out.println("Hello World!");
+
+        //Ausgabe "Julian Eisenmann" auf Konsole
+        System.out.println("Julian Eisenmann");
+
+        //Ausgabe einer leeren Zeile auf Konsole
+        System.out.println();
+
     }
 }
--- src/package2/Main2.java
+++ src/package2/Main2.java
@@ -1,4 +1,13 @@
+//Paket "package2"
 package package2;
 
-public class Main2 {
+//Erstellt eine Klasse Main2
+public class Main2
+{
+    //Erstellt die ausführbare Main Methode
+    public static void main (String [] args)
+    {
+        //Ausgabe "FH Kufstein Tirol" auf Konsole
+        System.out.println("FH Kufstein Tirol");
+    }
 }
