1af345dcfacf43c727e98bb1429635a4ace6c5eb
Julian Eisenmann <1810653115@stud.fh-kufstein.ac.at#>
2018-11-08 13:51:54 +0100

    neues Package/Klasse

---
+++ src/package3/Main3.java
@@ -0,0 +1,16 @@
+package package3;
+
+public class Main3
+{
+    public static void main (String[] args)
+    {
+        String name;
+        name = "Julian Eisenmann";
+        System.out.println(name);
+
+        int alter;
+        alter = 23;
+        System.out.println(alter);
+
+    }
+}
